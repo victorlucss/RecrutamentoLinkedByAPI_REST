f746b41cad55c42583f11fa525f8c039a5c3c064
Victor Silva - LinkedBy <victor.silva@controlinformatica.com.br>
2018-12-24 10:51:13 -0200

    Alterações na application para 'deployar' war

--- src/main/java/br/com/backend/Backend/BackEndApplication.java
+++ src/main/java/br/com/backend/Backend/BackEndApplication.java
@@ -2,12 +2,19 @@ package br.com.backend.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class BackEndApplication {
+public class BackEndApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BackEndApplication.class);
 	}
 }
